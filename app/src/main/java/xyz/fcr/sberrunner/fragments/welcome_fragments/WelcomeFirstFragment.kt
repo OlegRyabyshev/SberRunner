7d8ff1b35f7c28b0eedc1187726ae2c2b4c75a69
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-07-16 14:07:58 +0300

    bottomnav + changes in ui

--- app/src/main/java/xyz/fcr/sberrunner/fragments/welcome_fragments/WelcomeFirstFragment.kt
+++ app/src/main/java/xyz/fcr/sberrunner/fragments/welcome_fragments/WelcomeFirstFragment.kt
@@ -9,15 +9,10 @@ import xyz.fcr.sberrunner.R
 
 class WelcomeFirstFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome_first, container, false)
     }
 }
