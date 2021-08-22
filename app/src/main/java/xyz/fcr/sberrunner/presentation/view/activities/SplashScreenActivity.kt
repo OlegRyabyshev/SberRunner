ee615db6cb822e7e05a58bd5b129133623aee1f8
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-08-22 13:22:42 +0300

    kotlin redo splash screen

--- app/src/main/java/xyz/fcr/sberrunner/presentation/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/xyz/fcr/sberrunner/presentation/view/activities/SplashScreenActivity.kt
@@ -1,30 +1,26 @@
-package xyz.fcr.sberrunner.presentation.view.activities;
+package xyz.fcr.sberrunner.presentation.view.activities
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import android.os.Bundle
+import xyz.fcr.sberrunner.R
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
 
-import com.airbnb.lottie.LottieAnimationView;
+    private lateinit var lottie: LottieAnimationView
 
-import xyz.fcr.sberrunner.R;
-
-public class SplashScreenActivity extends AppCompatActivity {
-
-    LottieAnimationView lottie;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
-        lottie = findViewById(R.id.lottie_splash_screen);
-
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        }, 2000);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        lottie = findViewById(R.id.lottie_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }, 1500)
     }
 }
--- app/src/main/java/xyz/fcr/sberrunner/presentation/viewmodels/SingleLiveEvent.kt
+++ app/src/main/java/xyz/fcr/sberrunner/presentation/viewmodels/SingleLiveEvent.kt
@@ -1,21 +1,15 @@
 package xyz.fcr.sberrunner.presentation.viewmodels
 
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.atomic.AtomicBoolean
 
-//Кастомные класс-обертка
 class SingleLiveEvent<T> : MutableLiveData<T>() {
     private val mPending = AtomicBoolean(false)
 
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
-        if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
-        }
-        // Observe the internal MutableLiveData
         super.observe(owner, { t ->
             if (mPending.compareAndSet(true, false)) {
                 observer.onChanged(t)
@@ -27,9 +21,5 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
     override fun setValue(t: T?) {
         mPending.set(true)
         super.setValue(t)
-    }
-
-    companion object {
-        private const val TAG = "SingleLiveEvent"
     }
 }
