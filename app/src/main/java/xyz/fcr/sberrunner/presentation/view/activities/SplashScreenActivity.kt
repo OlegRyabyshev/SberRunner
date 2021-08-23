587fc31c8513c29b426200cce2b69ed2c4069d26
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-08-23 11:14:36 +0300

    changed splashscreen to picture

--- app/src/main/java/xyz/fcr/sberrunner/presentation/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/xyz/fcr/sberrunner/presentation/view/activities/SplashScreenActivity.kt
@@ -1,22 +1,16 @@
 package xyz.fcr.sberrunner.presentation.view.activities
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.lottie.LottieAnimationView
-import android.os.Bundle
-import xyz.fcr.sberrunner.R
+import android.app.Activity
 import android.content.Intent
-import android.os.Handler
-import android.os.Looper
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import xyz.fcr.sberrunner.presentation.App
 import javax.inject.Inject
-import android.app.Activity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
-
-    private lateinit var lottie: LottieAnimationView
 
     @Inject
     lateinit var firebaseAuth: FirebaseAuth
@@ -24,19 +18,15 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_splash_screen)
         App.appComponent.inject(this)
 
-        lottie = findViewById(R.id.lottie_splash_screen)
-        Handler(Looper.getMainLooper()).postDelayed({
-            when {
-                firebaseAuth.currentUser != null -> initActivity(MainActivity::class.java)
-                else -> initActivity(WelcomeActivity::class.java)
-            }
-        }, 2000)
+        when {
+            firebaseAuth.currentUser != null -> initActivity(MainActivity::class.java)
+            else -> initActivity(WelcomeActivity::class.java)
+        }
     }
 
-    private fun initActivity(activity: Class<out Activity>){
+    private fun initActivity(activity: Class<out Activity>) {
         val intent = Intent(this, activity)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
         startActivity(intent)
