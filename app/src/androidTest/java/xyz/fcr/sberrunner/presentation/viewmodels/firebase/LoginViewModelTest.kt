3b3180ac7e093c7d5b7e2779660ab8d5d81187a8
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-09-15 14:29:35 +0300

    presenter tests

---
+++ app/src/androidTest/java/xyz/fcr/sberrunner/presentation/viewmodels/firebase/LoginViewModelTest.kt
@@ -0,0 +1,98 @@
+package xyz.fcr.sberrunner.presentation.viewmodels.firebase
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import io.mockk.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import xyz.fcr.sberrunner.data.repository.shared.SharedPreferenceWrapper
+import xyz.fcr.sberrunner.domain.interactor.firebase.FirebaseInteractor
+import xyz.fcr.sberrunner.utils.Constants
+import xyz.fcr.sberrunner.utils.schedulers.SchedulersProvider
+
+@RunWith(JUnit4::class)
+class LoginViewModelTest {
+
+    @Rule
+    @JvmField
+    var rule = InstantTaskExecutorRule()
+
+    private val firebaseInteractor: FirebaseInteractor = mockk(relaxed = true)
+    private val schedulersProvider: SchedulersProvider = mockk(relaxed = true)
+    private val sharedPreferenceWrapper: SharedPreferenceWrapper = mockk(relaxed = true)
+
+    private val _progressLiveData: Observer<Boolean> = mockk()
+    private val _loginLiveData: Observer<Boolean> = mockk()
+    private val _resetLiveData: Observer<Boolean> = mockk()
+
+    private val _errorEmail: Observer<String> = mockk()
+    private val _errorPass: Observer<String> = mockk()
+    private val _errorFirebase: Observer<String> = mockk()
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    @Before
+    fun setUp() {
+        loginViewModel = LoginViewModel(
+            firebaseInteractor,
+            schedulersProvider,
+            sharedPreferenceWrapper
+        )
+
+        loginViewModel.progressLiveData.observeForever(_progressLiveData)
+        loginViewModel.loginLiveData.observeForever(_loginLiveData)
+        loginViewModel.resetLiveData.observeForever(_resetLiveData)
+
+        loginViewModel.errorEmail.observeForever(_errorEmail)
+        loginViewModel.errorPass.observeForever(_errorPass)
+        loginViewModel.errorFirebase.observeForever(_errorFirebase)
+
+        every { _progressLiveData.onChanged(any()) } just Runs
+        every { _loginLiveData.onChanged(any()) } just Runs
+        every { _resetLiveData.onChanged(any()) } just Runs
+
+        every { _errorEmail.onChanged(any()) } just Runs
+        every { _errorPass.onChanged(any()) } just Runs
+        every { _errorFirebase.onChanged(any()) } just Runs
+    }
+
+    @Test
+    fun assertSuccessfulEmailResetGivenValidEmail() {
+        loginViewModel.initResetEmail(VALID_EMAIL)
+
+        verify(exactly = 1) {
+            _errorEmail.onChanged(Constants.VALID)
+        }
+    }
+
+    @Test
+    fun assertNonSuccessfulEmailResetGivenNonValidEmail() {
+
+    }
+
+    @Test
+    fun assertSuccessfulLoginGivenValidEmailAndPassword() {
+
+    }
+
+    @Test
+    fun assertNonSuccessfulLoginGivenNonValidEmail() {
+
+    }
+
+    @Test
+    fun assertNonSuccessfulLoginGivenNonValidPassword() {
+
+    }
+
+    private companion object {
+        private const val VALID_EMAIL = "bob@gmail.com"
+        private const val VALID_PASS = "bobIsTheBest123"
+
+        private const val NON_VALID_EMAIL = "bob@"
+        private const val NON_VALID_PASS = "123"
+    }
+}
